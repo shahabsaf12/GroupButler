3a0a9f5ede5f441f44b758f8cf968e477da27106
Shahab <safaeishahab@yahoo.com>
2016-05-22 09:54:44 +0430

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
+ tar zxpf luarocks-2.2.2.tar.gz
+ cd luarocks-2.2.2
+ ./configure; sudo make bootstrap
+ sudo luarocks install luasocket
+ sudo luarocks install luasec
+ sudo luarocks install redis-lua
+ sudo luarocks install lua-term
+ sudo luarocks install serpent
+ cd ..
+ sudo apt-get install curl
+
+fi
